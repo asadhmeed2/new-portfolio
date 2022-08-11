689ed149c9cf51360c5f22f8f993d5ce4ed61745
newasad <asadhmeed2@gmail.com>
2022-08-11 23:17:59 +0300

    nav init style is ready

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import Logo from './logo.component'
-import './css/nav.style.css'
+import React from 'react';
+import NavList from './navList.component'
+import Logo from './logo.component';
+import './css/nav.style.css';
 
 function Nav() {
   return (
     <section className="nav">
         <Logo/>
+        <NavList/>
     </section>
   )
 }
---
+++ src/components/nav/navList.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './css/navList.style.css'
+
+const NavList = () => {
+  return (
+    <div>
+        <ul className="nav-list">
+            <li className="nav-list-item"><a href="/#">resume</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default NavList
