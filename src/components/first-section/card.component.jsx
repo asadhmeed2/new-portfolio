ab47b1c64afee904119a8cb5fc8632b2326b4675
newasad <asadhmeed2@gmail.com>
2022-08-12 23:30:47 +0300

    add portfolio card grid style

--- src/components/first-section/card.component.jsx
+++ src/components/first-section/card.component.jsx
@@ -4,7 +4,21 @@ import   "./css/card.style.css";
 const Card = () => {
   return (
     <section className="card">
-        card
+        <div className="portfolio-imag centered ">portfolio image</div>
+        <div className="name centered"><h1>Asaad Hmeed</h1></div>
+        <div className="contect centered">
+            <ul>
+                <li>Github</li>
+                <li>Linked</li>
+            </ul>
+            </div>
+        <div className="sumary centered">
+            <div>
+            Graduated from a Fullstack Bootcamp (intense training) by Applseeds. Deep knowledge in <strong>JS, HTML, CSS, React,Node.js,Express</strong>.<br/>
+            Highly motivated, and eager to learn. Able to learn independently, always seeking to expand skill set while using best code practices and creative thinking. <br/>
+            Looking for a challenging development position as a junior Full Stack Web Developer.<br/>
+            </div>
+</div>
     </section>
   )
 }
