47383d08b789df97319ff2e389f93e2bf30f1450
newasad <asadhmeed2@gmail.com>
2022-08-13 00:42:43 +0300

    .,

--- src/components/first-section/card.component.jsx
+++ src/components/first-section/card.component.jsx
@@ -4,21 +4,21 @@ import   "./css/card.style.css";
 const Card = () => {
   return (
     <section className="card">
-        <div className="portfolio-imag centered ">portfolio image</div>
-        <div className="name centered"><h1>Asaad Hmeed</h1></div>
-        <div className="contect centered">
+        <div className="centered portfolio-imag">portfolio image</div>
+        <div className="centered name"><h1>Asaad Hmeed</h1></div>
+        <div className="centered contect">
             <ul>
                 <li>Github</li>
                 <li>Linked</li>
             </ul>
-            </div>
-        <div className="sumary centered">
+        </div>
+        <div className="centered sumary">
             <div>
             Graduated from a Fullstack Bootcamp (intense training) by Applseeds. Deep knowledge in <strong>JS, HTML, CSS, React,Node.js,Express</strong>.<br/>
             Highly motivated, and eager to learn. Able to learn independently, always seeking to expand skill set while using best code practices and creative thinking. <br/>
             Looking for a challenging development position as a junior Full Stack Web Developer.<br/>
             </div>
-</div>
+        </div>
     </section>
   )
 }
