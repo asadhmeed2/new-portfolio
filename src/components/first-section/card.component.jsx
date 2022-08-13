4e9095feabd28cdde75e76d2fd48a2c7e30de1b9
newasad <asadhmeed2@gmail.com>
2022-08-13 03:02:56 +0300

    ..

--- src/components/first-section/card.component.jsx
+++ src/components/first-section/card.component.jsx
@@ -4,15 +4,15 @@ import   "./css/card.style.css";
 const Card = () => {
   return (
     <div className="card">
-        <div className="centered portfolio-imag">portfolio image</div>
-        <div className="centered name"><h1>Asaad Hmeed</h1></div>
-        <div className="centered contect">
+        <div className="centered card-border portfolio-imag">portfolio image</div>
+        <div className="centered card-border name"><h1>Asaad Hmeed</h1></div>
+        <div className="centered card-border contect">
             <ul className="">
                 <li>Github</li>
                 <li>Linked</li>
             </ul>
         </div>
-        <div className="centered sumary">
+        <div className="centered card-border sumary">
             <div>
             Graduated from a Fullstack Bootcamp (intense training) by Applseeds. Deep knowledge in <strong>JS, HTML, CSS, React,Node.js,Express</strong>.<br/>
             Highly motivated, and eager to learn. Able to learn independently, always seeking to expand skill set while using best code practices and creative thinking. <br/>
