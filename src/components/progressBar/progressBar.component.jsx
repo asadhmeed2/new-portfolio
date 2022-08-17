138331b84a6337d776143a8556d5484b7ba10cee
newasad <asadhmeed2@gmail.com>
2022-08-17 21:58:04 +0300

    init progress bar

---
+++ src/components/progressBar/progressBar.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/ProgressBar.style.css'
+
+const ProgressBar = () => {
+  return (
+    <div className="progress-bar">
+      
+    </div>
+  )
+}
+
+export default ProgressBar
