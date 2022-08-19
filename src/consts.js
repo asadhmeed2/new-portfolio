6c3de89d13b6a4e1f218ab340e0e70ba8a487e06
newasad <asadhmeed2@gmail.com>
2022-08-19 21:15:50 +0300

    ..

--- src/consts.js
+++ src/consts.js
@@ -1,2 +1,3 @@
 export const LINKEDIN_URL = "https://www.linkedin.com/in/asad-hmeed-173a46130";
 export const GITHUB_URL = "https://github.com/newasad";
+export const FRAMEWORK_PROGRESS_BAR_DATA = [{id:1,fromColor:"",toColor:""},]
