9ef229a5b8f4d31445bb7e60b7a83fffa8cea61c
newasad <asadhmeed2@gmail.com>
2022-08-21 18:51:32 +0300

    add projects file and changed the progressBar number color functionality

--- src/components/progressBar/progressBar.component.jsx
+++ src/components/progressBar/progressBar.component.jsx
@@ -6,7 +6,7 @@ const ProgressBar = ({fromColor="#DA22FF", toColor="#9733EE", number=65}) => {
   return (
     <div className="progress-bar">
     <div className="progress-bar-outer">
-      <div className="progress-bar-inner" style={{color: fromColor}}>
+      <div className="progress-bar-inner" style={{color: `${number > 70?'green':number >30? 'orange':'red' }`}}>
         <div className="number">{`${number}`}%</div>
       </div>
     </div>
